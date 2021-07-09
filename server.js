b506fdbea2b5751d2a4828b87f83633c40a38e5d
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-07-09 16:56:17 +0900

    Database connection

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
-
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to DB
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
 
-app.use(logger);
+// Dev loggin middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
 });
