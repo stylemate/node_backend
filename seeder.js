7cb7b070a9bcc73a5c770c11e0d19917b63163f6
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-07-13 16:45:38 +0900

    Data seeder

---
+++ seeder.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const Bootcamp = require("./models/Bootcamp");
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+//Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+        console.log("Data Imported...".green.inverse);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//Delete Data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log("Data Destroyed...".red.inverse);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+if (process.argv[2] === "-i") {
+    importData();
+} else if (process.argv[2] === "-d") {
+    deleteData();
+}
