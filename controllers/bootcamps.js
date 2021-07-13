37ea105a7c11960a013622277d682d95d5ebc4e2
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-07-13 17:17:43 +0900

    Geospatial Query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
+const geocoder = require("../utils/geocoder");
 const Bootcamp = require("../models/Bootcamp");
 
 // @desc        Get all bootcamps
@@ -11,7 +12,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     res.status(200).json({
         success: true,
         count: bootcamps.length,
-        data: bootcamps
+        data: bootcamps,
     });
 });
 
@@ -48,7 +49,7 @@ exports.postBootcamp = asyncHandler(async (req, res, next) => {
 exports.putBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
         new: true, //updated data will be displayed
-        runValidators: true //mongoose validator
+        runValidators: true, //mongoose validator
     });
 
     if (!bootcamp) {
@@ -62,7 +63,7 @@ exports.putBootcamp = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        data: bootcamp
+        data: bootcamp,
     });
 });
 
@@ -83,6 +84,36 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        data: {}
+        data: {},
+    });
+});
+
+// @desc        Get bootcamps within a radius
+// @route       GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access      Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    // Calc radius using radians
+    // Divide distance by radius of Earth
+    // Earth Radius = 3,963 mi or 6,378 km
+    const radius = distance / 6378;
+
+    const bootcamps = await Bootcamp.find({
+        location: {
+            $geoWithin: {
+                $centerSphere: [[lng, lat], radius],
+            },
+        },
+    });
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps,
     });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,8 +5,11 @@ const {
     postBootcamp,
     putBootcamp,
     deleteBootcamp,
+    getBootcampsInRadius,
 } = require("../controllers/bootcamps");
 const router = express.Router();
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router.route("/").get(getBootcamps).post(postBootcamp);
 
