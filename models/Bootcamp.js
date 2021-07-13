06f36b8e0a004c804b60c5817ce188cf3e16bf1a
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-07-13 16:00:51 +0900

    Mongoose Hook, Geocoder middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -103,6 +104,24 @@ const BootcampSchema = new mongoose.Schema({
 // Arrow function handles 'this' keyword differently
 BootcampSchema.pre("save", function (next) {
     this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// Geocode & Create location field
+BootcampSchema.pre("save", async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: "Point",
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    };
+
+    this.address = undefined;
     next();
 });
 
