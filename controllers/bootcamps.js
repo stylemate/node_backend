29122de69460cb02d66801978798f010201e11fc
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-07-19 16:48:10 +0900

    Select & Sort in query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,21 +9,47 @@ const Bootcamp = require("../models/Bootcamp");
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query;
 
-    let queryString = JSON.stringify(req.query);
+    // Copy req.query
+    const reqQuery = { ...req.query };
 
+    // Fields to exclude
+    const removeFields = ["select", "sort"];
+
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach((param) => delete reqQuery[param]);
+
+    // Create query string
+    let queryString = JSON.stringify(reqQuery);
+
+    // Create operators like ($gt, $gte, etc)
     queryString = queryString.replace(
         /\b(gt|gte|lt|lte|in)\b/g,
         (match) => `$${match}`
     );
 
+    // Finding resource
     query = Bootcamp.find(JSON.parse(queryString));
 
+    // Select Fields
+    if (req.query.select) {
+        const fields = req.query.select.split(",").join(" ");
+        query = query.select(fields);
+    }
+
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(",").join(" ");
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort("-createdAt");
+    }
+
+    // Executing query
     const bootcamps = await query;
 
     res.status(200).json({
         success: true,
         count: bootcamps.length,
-        data: bootcamps
+        data: bootcamps,
     });
 });
 
@@ -60,7 +86,7 @@ exports.postBootcamp = asyncHandler(async (req, res, next) => {
 exports.putBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
         new: true, //updated data will be displayed
-        runValidators: true //mongoose validator
+        runValidators: true, //mongoose validator
     });
 
     if (!bootcamp) {
@@ -74,7 +100,7 @@ exports.putBootcamp = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        data: bootcamp
+        data: bootcamp,
     });
 });
 
@@ -95,7 +121,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        data: {}
+        data: {},
     });
 });
 
@@ -117,14 +143,14 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     const bootcamps = await Bootcamp.find({
         location: {
             $geoWithin: {
-                $centerSphere: [[lng, lat], radius]
-            }
-        }
+                $centerSphere: [[lng, lat], radius],
+            },
+        },
     });
 
     res.status(200).json({
         success: true,
         count: bootcamps.length,
-        data: bootcamps
+        data: bootcamps,
     });
 });
