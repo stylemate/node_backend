8cf607209c0af90c95f739bf3093fa774556c19d
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-07-30 17:50:48 +0900

    Refactoring advanced query results into a middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,19 +8,17 @@ const Bootcamp = require("../models/Bootcamp");
 // @route       GET /api/v1/bootcamps/:bootcampId/courses
 // @access      Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    let query;
-
-    if (req.params.bootcampId)
-        query = Course.find({ bootcamp: req.params.bootcampId });
-    else query = Course.find();
-
-    const courses = await query;
-
-    res.status(200).json({
-        success: true,
-        count: courses.length,
-        data: courses
-    });
+    if (req.params.bootcampId) {
+        const courses = await Course.find({ bootcamp: req.params.bootcampId });
+        //if we're getting courses for specific bootcamp, we don't really need pagination, etc.
+        return res.status(200).json({
+            success: true,
+            count: courses.length,
+            data: courses
+        });
+    } else {
+        res.status(200).json(res.advancedResults);
+    }
 });
 
 // @desc        Get a single course
--- routes/courses.js
+++ routes/courses.js
@@ -6,9 +6,22 @@ const {
     putCourse,
     deleteCourse
 } = require("../controllers/courses");
+
+const Course = require("../models/Course");
+const advancedResults = require("../middleware/advancedResults");
+
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getCourses).post(postCourse);
+router
+    .route("/")
+    .get(
+        advancedResults(Course, {
+            path: "bootcamp",
+            select: "name description"
+        }),
+        getCourses
+    )
+    .post(postCourse);
 
 router.route("/:id").get(getCourse).put(putCourse).delete(deleteCourse);
 
